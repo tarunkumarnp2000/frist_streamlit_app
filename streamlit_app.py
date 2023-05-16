c0587cd1159b16c572a72d4d8c74b09ddb71eb62
tarunkumarnp2000 <114480765+tarunkumarnp2000@users.noreply.github.com>
2023-05-16 10:39:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,6 @@ streamlit.text(' 🥣 omega 3 bluberry oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
