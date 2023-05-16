560cfa70560ad1e2a1fd7be19f17d691e89607b9
tarunkumarnp2000 <114480765+tarunkumarnp2000@users.noreply.github.com>
2023-05-16 10:25:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.header('tarun')
 streamlit.title('my parents new healthy diner')
-streamlt.title('my parents new healthy diner')
+streamlit.title('my parents new healthy diner')
