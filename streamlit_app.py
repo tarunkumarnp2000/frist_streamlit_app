181cc90e505724110678e39e222fde37dc41ae85
tarunkumarnp2000 <114480765+tarunkumarnp2000@users.noreply.github.com>
2023-05-16 10:29:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 streamlit.header('tarun')
 streamlit.title('my parents new healthy diner')
 streamlit.title('my parents new healthy diner')
+streamlit.text('breakfast menu')
+streamlit.text('omega 3 bluberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
